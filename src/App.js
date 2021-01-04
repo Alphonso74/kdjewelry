4464ea3f1ac2a49d45f179b752f3d3e3711cd8dc
Alphonso74 <ajm6684@psu.edu>
2021-01-04 15:10:00 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import {Route, Switch, withRouter} from 'react-router-dom';
+import MainPage from "./Components/MainPage";
+import Catalog from './Components/Catalog';
+import MyOrder from './Components/MyOrder';
+import Contact from './Components/Contact';
+
+class App extends Component {
+
+    state = {
+
+        currentComponent: null
+    };
+
+    render(){
+
+        let routes = (
+
+            <Switch>
+
+                {/*<Route path="/auth"  component={}/>*/}
+                <Route path="/" exact component={MainPage}/>
+                <Route path="/Catalog" exact component={Catalog}/>
+                <Route path="/MyOrder" exact component={MyOrder}/>
+                <Route path="/Contact" exact component={Contact}/>
+
+
+
+
+                {/*<Redirect to="/"/>*/}
+
+            </Switch>
+
+        );
+
+        return (
+            <div>
+
+
+
+                {routes}
+
+
+
+            </div>
+        );
+    }
+}
+
+export default withRouter(App);
---
+++ src/Components/MyOrder.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+
+class MyOrder extends Component {
+
+
+    render(){
+
+
+
+        return (
+            <div>
+
+
+                <h1>My Order</h1>
+
+
+
+            </div>
+        );
+    }
+}
+
+export default MyOrder;
